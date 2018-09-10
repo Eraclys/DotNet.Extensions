70cd4f9a108a81bd453817709744443aed5615f4
Milian Lichere <milian.lichere@tekgent.com>
2018-09-10 16:41:00 +0100

    added Else extensions

---
+++ DotNetExtensions.Test/NullExtensions_Else_Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNetExtensions.Test
+{
+    [TestFixture]
+    internal sealed class NullExtensions_Else_Tests
+    {
+        [FsCheck.NUnit.Property]
+        public void GivenANullObject_ShouldReturnAlternative(object alternative)
+        {
+            ((object)null).Else(alternative).Should().Be(alternative);
+            ((object)null).Else(() => alternative).Should().Be(alternative);
+        }
+
+        [FsCheck.NUnit.Property]
+        public void GivenANonNullObject_ShouldReturnOriginal(object originalValue, object alternative)
+        {
+            if (originalValue == null)
+            {
+                return;
+            }
+
+            originalValue.Else(alternative).Should().Be(originalValue);
+            originalValue.Else(() => alternative).Should().Be(originalValue);
+        }
+
+        [FsCheck.NUnit.Property]
+        public void GivenANullNullableStruct_ShouldReturnAlternative(int? alternative)
+        {
+            ((int?)null).Else(alternative).Should().Be(alternative);
+            ((int?)null).Else(() => alternative).Should().Be(alternative);
+        }
+
+        [FsCheck.NUnit.Property]
+        public void GivenANonNullNullableStruct_ShouldReturnOriginal(int? originalValue, int? alternative)
+        {
+            if (originalValue == null)
+            {
+                return;
+            }
+
+            originalValue.Else(alternative).Should().Be(originalValue);
+            originalValue.Else(() => alternative).Should().Be(originalValue);
+        }
+    }
+}
--- DotNetExtensions/NullObjectExtensions.cs
+++ DotNetExtensions/NullObjectExtensions.cs
@@ -5,7 +5,7 @@ namespace DotNetExtensions
 {
     public static class NullObjectExtensions
     {
-        public static void IfNotNullThenDo<TObject>([CanBeNull] this TObject value, Action<TObject> action)
+        public static void IfNotNullThenDo<TObject>([CanBeNull] this TObject value, [NotNull] Action<TObject> action)
             where TObject : class
         {
             if (value != null)
@@ -14,7 +14,7 @@ namespace DotNetExtensions
             }
         }
 
-        public static void IfHasValueThenDo<TValue>([CanBeNull] this TValue? value, Action<TValue> action)
+        public static void IfHasValueThenDo<TValue>([CanBeNull] this TValue? value, [NotNull] Action<TValue> action)
             where TValue : struct
         {
             if (value.HasValue)
@@ -24,17 +24,45 @@ namespace DotNetExtensions
         }
 
         [CanBeNull]
-        public static TResult IfNotNull<TObject, TResult>([CanBeNull] this TObject value, Func<TObject, TResult> selector)
+        public static TResult IfNotNull<TObject, TResult>([CanBeNull] this TObject value, [NotNull] Func<TObject, TResult> selector)
             where TObject : class
         {
             return value != null ? selector(value) : default(TResult);
         }
 
         [CanBeNull]
-        public static TResult IfNotNull<TValue, TResult>([CanBeNull] this TValue? value, Func<TValue, TResult> selector)
+        public static TResult IfNotNull<TValue, TResult>([CanBeNull] this TValue? value, [NotNull] Func<TValue, TResult> selector)
             where TValue : struct
         {
             return value.HasValue ? selector(value.Value) : default(TResult);
+        }
+
+        [CanBeNull]
+        public static TObject Else<TObject>([CanBeNull] this TObject value, [CanBeNull] TObject alternative)
+            where TObject : class
+        {
+            return value ?? alternative;
+        }
+
+        [CanBeNull]
+        public static TObject Else<TObject>([CanBeNull] this TObject value, [NotNull] Func<TObject> getAlternative)
+            where TObject : class
+        {
+            return value ?? getAlternative();
+        }
+
+        [CanBeNull]
+        public static TValue? Else<TValue>([CanBeNull] this TValue? value, [CanBeNull] TValue? alternative)
+            where TValue : struct
+        {
+            return value ?? alternative;
+        }
+
+        [CanBeNull]
+        public static TValue? Else<TValue>([CanBeNull] this TValue? value, [NotNull] Func<TValue?> getAlternative)
+            where TValue : struct
+        {
+            return value ?? getAlternative();
         }
     }
 }
