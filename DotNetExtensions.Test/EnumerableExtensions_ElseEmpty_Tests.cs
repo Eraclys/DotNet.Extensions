322ddb366f779e4889c9c3981f88e58bf230c41a
Milian Lichere <milian.lichere@tekgent.com>
2018-09-10 17:26:25 +0100

    added ElseEmpty extension to IEnumerable

---
+++ DotNetExtensions.Test/EnumerableExtensions_ElseEmpty_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNetExtensions.Test
+{
+    [TestFixture]
+    internal sealed class EnumerableExtensions_ElseEmpty_Tests
+    {
+        [Test]
+        public void GivenNull_ShouldReturnEmptyEnumerable()
+        {
+            var result = ((IEnumerable<int>)null).ElseEmpty();
+
+            result.Should().NotBeNull();
+            result.Should().BeEmpty();
+        }
+
+        [FsCheck.NUnit.Property]
+        public void GivenNonNull_ShouldReturnOriginal(List<int> items)
+        {
+            if (items == null)
+            {
+                return;
+            }
+
+            var result = items.ElseEmpty();
+
+            result.Should().BeSameAs(items);
+        }
+    }
+}
