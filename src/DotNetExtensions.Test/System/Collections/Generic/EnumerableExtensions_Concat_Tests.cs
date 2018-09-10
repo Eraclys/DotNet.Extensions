a7b7470a8e7df5a1834f688e9b1dc196eb167652
Milian Lichere <milian.lichere@tekgent.com>
2018-09-10 21:03:44 +0100

    renamed ConcatOne to append and added Prepend extension

---
+++ src/DotNetExtensions.Test/System/Collections/Generic/EnumerableExtensions_Concat_Tests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DotNetExtensions.System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNetExtensions.Test.System.Collections.Generic
+{
+    [TestFixture]
+    internal sealed class EnumerableExtensions_Concat_Tests
+    {
+        [FsCheck.NUnit.Property]
+        public void GivenNull_Append_ShouldThrowException(object concatValue)
+        {
+            Assert.Throws<ArgumentNullException>(() => { ((IEnumerable<object>) null).Append(concatValue).ToList(); });
+        }
+
+        [FsCheck.NUnit.Property]
+        public void GivenNonNull_Append_ShouldReturnOriginalPlusItemAtTheEndOfTheCollection(List<object> items, object item)
+        {
+            if (items == null)
+            {
+                return;
+            }
+
+            var result = items.Append(item);
+
+            result.Should().BeEquivalentTo(items.Concat(new []{item}));
+        }
+
+        [FsCheck.NUnit.Property]
+        public void GivenNull_Prepend_ShouldThrowException(object concatValue)
+        {
+            Assert.Throws<ArgumentNullException>(() => { ((IEnumerable<object>)null).Prepend(concatValue).ToList(); });
+        }
+
+        [FsCheck.NUnit.Property]
+        public void GivenNonNull_Prepend_ShouldReturnOriginalPlusItemAtTheBeginningOfTheCollection(List<object> items, object item)
+        {
+            if (items == null)
+            {
+                return;
+            }
+
+            var result = items.Prepend(item);
+
+            result.Should().BeEquivalentTo(new[] { item }.Concat(items));
+        }
+    }
+}
---
+++ src/DotNetExtensions/System/Collections/Generic/EnumerableConcat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace DotNetExtensions.System.Collections.Generic
+{
+    public static class EnumerableConcat
+    {
+        [NotNull]
+        public static IEnumerable<T> Append<T>([NotNull] this IEnumerable<T> items, T value)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            foreach (var item in items)
+            {
+                yield return item;
+            }
+
+            yield return value;
+        }
+
+        [NotNull]
+        public static IEnumerable<T> Prepend<T>([NotNull] this IEnumerable<T> items, T value)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            yield return value;
+
+            foreach (var item in items)
+            {
+                yield return item;
+            }
+        }
+    }
+}
