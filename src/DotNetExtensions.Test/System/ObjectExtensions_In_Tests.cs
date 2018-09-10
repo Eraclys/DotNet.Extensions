d8ee842fb5a525334206eec91daa901fe26fdae6
Milian Lichere <milian.lichere@tekgent.com>
2018-09-10 21:14:35 +0100

    added In extensions

---
+++ src/DotNetExtensions.Test/System/ObjectExtensions_In_Tests.cs
@@ -0,0 +1,24 @@
+﻿using DotNetExtensions.System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNetExtensions.Test.System
+{
+    [TestFixture]
+    internal sealed class ObjectExtensions_In_Tests
+    {
+        [Test]
+        public void WhenObjectIsInCollection_ReturnTrue()
+        {
+            "b".In("a","b", "c").Should().BeTrue();
+            5.In(5, 6, 9, -1).Should().BeTrue();
+        }
+
+        [Test]
+        public void WhenObjectIsNotInCollection_ReturnFalse()
+        {
+            "b".In("a", "c").Should().BeFalse();
+            5.In(6, 9, -1).Should().BeFalse();
+        }
+    }
+}
---
+++ src/DotNetExtensions/System/ObjectInExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace DotNetExtensions.System
+{
+    public static class ObjectInExtensions
+    {
+        public static bool In<T>(this T value, params T[] values)
+        {
+            return values != null && values.Contains(value);
+        }
+    }
+}
