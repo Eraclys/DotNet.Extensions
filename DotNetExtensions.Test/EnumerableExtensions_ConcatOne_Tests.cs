6ae4b087a68c1e64c32481376b16beaec347aaf0
Milian Lichere <milian.lichere@tekgent.com>
2018-09-10 18:17:01 +0100

    added ConcatOne extension to ienumerable

---
+++ DotNetExtensions.Test/EnumerableExtensions_ConcatOne_Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNetExtensions.Test
+{
+    [TestFixture]
+    internal sealed class EnumerableExtensions_ConcatOne_Tests
+    {
+        [FsCheck.NUnit.Property]
+        public void GivenNull_ShouldThrowException(int? concatValue)
+        {
+            Assert.Throws<ArgumentNullException>(() => { ((IEnumerable<int?>) null).ConcatOne(concatValue); });
+        }
+
+        [FsCheck.NUnit.Property]
+        public void GivenNonNull_ShouldReturnOriginal(List<int?> items, int? item)
+        {
+            if (items == null)
+            {
+                return;
+            }
+
+            var result = items.ConcatOne(item);
+
+            result.Should().BeEquivalentTo(items.Concat(new []{item}));
+        }
+    }
+}
---
+++ DotNetExtensions/EnumerableConcatOne.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace DotNetExtensions
+{
+    public static class EnumerableConcatOne
+    {
+        [NotNull]
+        public static IEnumerable<T> ConcatOne<T>([NotNull] this IEnumerable<T> items, T item)
+        {
+            return items.Concat(new[] {item});
+        }
+    }
+}
