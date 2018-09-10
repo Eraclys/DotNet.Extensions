adafef6db380c34c32da40b9f8f5ef280d3b6448
Milian Lichere <milian.lichere@tekgent.com>
2018-09-10 16:19:38 +0100

    added some NullObjectExtensions

---
+++ DotNetExtensions.Test/NullExtensions_IfNotNullThenDo_Tests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace DotNetExtensions.Test
+{
+    [TestFixture]
+    internal sealed class NullExtensions_IfNotNullThenDo_Tests
+    {
+        [Test]
+        public void GivenANullObject_ShouldNotCallAction()
+        {
+            var callCount = 0;
+
+            ((object)null).IfNotNullThenDo(v => callCount++);
+
+            callCount.Should().Be(0);
+        }
+
+        [Test]
+        public void GivenANonNullObject_ShouldCallAction()
+        {
+            var callCount = 0;
+
+            var @object = new object();
+            object value = null;
+
+            @object.IfNotNullThenDo(v =>
+            {
+                value = v;
+                callCount++;
+            });
+
+            value.Should().Be(@object);
+            callCount.Should().Be(1);
+        }
+
+        [Test]
+        public void GivenANullNullableStruct_ShouldNotCallAction()
+        {
+            var callCount = 0;
+
+            ((int?)null).IfHasValueThenDo(v => callCount++);
+
+            callCount.Should().Be(0);
+        }
+
+        [Test]
+        public void GivenANonNullNullableStruct_ShouldCallAction()
+        {
+            var callCount = 0;
+
+            int? nullableStruct = 1;
+            var value = 0;
+
+            nullableStruct.IfHasValueThenDo(v =>
+            {
+                value = v;
+                callCount++;
+            });
+
+            value.Should().Be(nullableStruct.Value);
+            callCount.Should().Be(1);
+        }
+    }
+}
---
+++ DotNetExtensions/NullObjectExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace DotNetExtensions
+{
+    public static class NullObjectExtensions
+    {
+        public static void IfNotNullThenDo<TObject>([CanBeNull] this TObject value, Action<TObject> action)
+            where TObject : class
+        {
+            if (value != null)
+            {
+                action(value);
+            }
+        }
+
+        public static void IfHasValueThenDo<TValue>([CanBeNull] this TValue? value, Action<TValue> action)
+            where TValue : struct
+        {
+            if (value.HasValue)
+            {
+                action(value.Value);
+            }
+        }
+
+        [CanBeNull]
+        public static TResult IfNotNull<TObject, TResult>([CanBeNull] this TObject value, Func<TObject, TResult> selector)
+            where TObject : class
+        {
+            return value != null ? selector(value) : default(TResult);
+        }
+
+        [CanBeNull]
+        public static TResult IfNotNull<TValue, TResult>([CanBeNull] this TValue? value, Func<TValue, TResult> selector)
+            where TValue : struct
+        {
+            return value.HasValue ? selector(value.Value) : default(TResult);
+        }
+    }
+}
