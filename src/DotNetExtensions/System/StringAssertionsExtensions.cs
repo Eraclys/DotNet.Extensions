98ed246008fa2789921f7164feb60271553a881c
Milian Lichere <milian.lichere@tekgent.com>
2018-09-11 11:23:38 +0100

    added DefaultIfNullOrEmpty and DefaultIfNullOrWhiteSpace on strings

--- src/DotNetExtensions/System/StringAssertionsExtensions.cs
+++ src/DotNetExtensions/System/StringAssertionsExtensions.cs
@@ -4,11 +4,13 @@ namespace DotNetExtensions.System
 {
     public static class StringAssertionsExtensions
     {
+        [ContractAnnotation("value:null => true")]
         public static bool IsNullOrEmpty([CanBeNull] this string value)
         {
             return string.IsNullOrEmpty(value);
         }
 
+        [ContractAnnotation("value:null => true")]
         public static bool IsNullOrWhiteSpace([CanBeNull] this string value)
         {
             return string.IsNullOrWhiteSpace(value);
---
+++ src/DotNetExtensions/System/StringDefaultExtensions.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+
+namespace DotNetExtensions.System
+{
+    public static class StringDefaultExtensions
+    {
+        [NotNull]
+        public static string DefaultIfNullOrEmpty([CanBeNull] this string value, string @defaultValue)
+        {
+            return string.IsNullOrEmpty(value) ? @defaultValue : value;
+        }
+
+        [NotNull]
+        public static string DefaultIfNullOrWhiteSpace([CanBeNull] this string value, string @defaultValue)
+        {
+            return string.IsNullOrWhiteSpace(value) ? @defaultValue : value;
+        }
+    }
+}
