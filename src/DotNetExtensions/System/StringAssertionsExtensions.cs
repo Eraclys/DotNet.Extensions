bcc1d3b9a9b51ffb09216851f0c5c1a2052af874
Milian Lichere <milian.lichere@tekgent.com>
2018-09-11 11:07:15 +0100

    added some string assertions extensions

---
+++ src/DotNetExtensions/System/StringAssertionsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using JetBrains.Annotations;
+
+namespace DotNetExtensions.System
+{
+    public static class StringAssertionsExtensions
+    {
+        public static bool IsNullOrEmpty([CanBeNull] this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        public static bool IsNullOrWhiteSpace([CanBeNull] this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+    }
+}
