48552870b1684f794ca6af1d52a7e6bcabbfab48
Milian Lichere <milian.lichere@tekgent.com>
2018-09-12 11:19:17 +0100

    added Slice extension to arrays

---
+++ src/DotNetStandard.Extensions.Test/System.T[]/T[].Slice_Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+// ReSharper disable CheckNamespace
+
+namespace DotNetStandard.Extensions.Test
+{
+    [TestFixture]
+    internal sealed class ArrayExtensions_Slice_Tests
+    {
+        [FsCheck.NUnit.Property]
+        public void WhenANull_ShouldReturnEmptyArray(int index)
+        {
+            var fromIndex = Math.Abs(index);
+
+            ((int[]) null).Slice(fromIndex).Should().BeEmpty();
+        }
+
+        [FsCheck.NUnit.Property]
+        public void WhenFromIndexIsNegative_ShouldThrowException(int[] array, int index)
+        {
+            var fromIndex = (-Math.Abs(index)).WithMaxValueOf(-1);
+
+            Assert.Throws<ArgumentException>(() => { array.Slice(fromIndex); });
+        }
+
+        [FsCheck.NUnit.Property]
+        public void WhenFromIndexIsGreaterThanToIndex_ShouldThrowException(int[] array, int index)
+        {
+            var fromIndex = Math.Abs(index);
+            var toIndex = fromIndex - 1;
+
+            Assert.Throws<ArgumentException>(() => { array.Slice(fromIndex, toIndex); });
+        }
+
+        [Test]
+        public void ShouldReturnSubsetOfArray()
+        {
+            new[] {'a', 'b', 'c', 'd'}.Slice(0, 2).Should().BeEquivalentTo(new[] {'a', 'b'});
+            new[] {'a', 'b', 'c', 'd'}.Slice(1, 3).Should().BeEquivalentTo(new[] {'b', 'c'});
+            new[] {'a', 'b', 'c', 'd'}.Slice(1).Should().BeEquivalentTo(new[] {'b', 'c', 'd'});
+            new[] {'a', 'b', 'c', 'd'}.Slice(0).Should().BeEquivalentTo(new[] {'a', 'b', 'c', 'd'});
+        }
+    }
+}
---
+++ src/DotNetStandard.Extensions/System.T[]/T[].Slice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace DotNetStandard.Extensions
+{
+    public static partial class ArrayExtensions
+    {
+        [NotNull]
+        public static T[] Slice<T>([CanBeNull] this T[] items, int fromIndex, int toIndex = int.MaxValue)
+        {
+            if (fromIndex < 0)
+            {
+                throw new ArgumentException("fromIndex must be positive", nameof(fromIndex));
+            }
+
+            if (fromIndex > toIndex)
+            {
+                throw new ArgumentException("fromIndex is greater than to Index", nameof(fromIndex));
+            }
+
+            if (items == null)
+            {
+                return new T[0];
+            }
+
+            toIndex = toIndex.WithMaxValueOf(items.Length);
+            var length = toIndex - fromIndex;
+            var result = new T[length];
+
+            Array.Copy(items, fromIndex, result, 0, length);
+
+            return result;
+        }
+    }
+}
