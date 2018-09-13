cc9511c3e521992cc6da89ef1b874a170d4eab16
Milian Lichere <milian.lichere@tekgent.com>
2018-09-13 18:28:43 +0100

    Added IDictionary[TKey,TValue].GetValueOrAdd

---
+++ src/DotNetStandard.Extensions.Test/System.Collections.Generic.IDictionary[Tkey,TValue]/IDictionary[TKey,TValue].GetValueOrAdd_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using FsCheck;
+using NUnit.Framework;
+// ReSharper disable CheckNamespace
+
+namespace DotNetStandard.Extensions.Test
+{
+    [TestFixture]
+    internal sealed class DictionaryExtensions_GetValueOrAdd_Tests
+    {
+        [FsCheck.NUnit.Property]
+        public void WhenKeyDoesNotExist_CreateNewDictionaryEntry(NonNull<string> nonNullKey, int onCreateValue)
+        {
+            var key = nonNullKey.Item;
+
+            var dictionary = new Dictionary<string, int>();
+
+            var result = dictionary.GetValueOrAdd(key, () => onCreateValue);
+
+            result.Should().Be(onCreateValue);
+            dictionary.ContainsKey(key).Should().BeTrue();
+            dictionary[key].Should().Be(onCreateValue);
+        }
+
+        [FsCheck.NUnit.Property]
+        public void WhenKeyExists_ShouldReturnValue(NonNull<string> nonNullKey, int originalValue, int onCreateValue)
+        {
+            var key = nonNullKey.Item;
+
+            var dictionary = new Dictionary<string, int> { { key, originalValue } };
+
+            var result = dictionary.GetValueOrAdd(key, () => onCreateValue);
+
+            result.Should().Be(originalValue);
+            dictionary.ContainsKey(key).Should().BeTrue();
+            dictionary[key].Should().Be(originalValue);
+        }
+    }
+}
---
+++ src/DotNetStandard.Extensions/System.Collections.Generic.IDictionary[Tkey,TValue]/IDictionary[TKey,TValue].GetValueOrAdd.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+// ReSharper disable CheckNamespace
+
+namespace DotNetStandard.Extensions
+{
+    public static partial class DictionaryExtensions
+    {
+        [NotNull]
+        public static TValue GetValueOrAdd<TKey, TValue>(
+            [NotNull] this IDictionary<TKey, TValue> dictionary,
+            [NotNull] TKey key,
+            [NotNull] Func<TValue> createFunc)
+        {
+            if (!dictionary.TryGetValue(key, out var value))
+            {
+                dictionary[key] = value = createFunc();
+            }
+
+            return value;
+        }
+    }
+}
