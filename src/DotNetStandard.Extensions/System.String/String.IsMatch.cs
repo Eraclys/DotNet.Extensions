b6c31f8c74b094601c93627d7e6979c43c39f1ce
Milian Lichere <milian.lichere@tekgent.com>
2018-09-14 10:38:05 +0100

    added IsMatch

---
+++ src/DotNetStandard.Extensions/System.String/String.IsMatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using JetBrains.Annotations;
+// ReSharper disable CheckNamespace
+
+namespace DotNetStandard.Extensions
+{
+    public static partial class StringExtensions
+    {
+        public static bool IsMatch([CanBeNull] this string value, [NotNull] string pattern)
+        {
+            return value != null && Regex.IsMatch(value, pattern);
+        }
+
+        public static bool IsMatch([CanBeNull] this string value, [NotNull] string pattern, RegexOptions options)
+        {
+            return value != null && Regex.IsMatch(value, pattern, options);
+        }
+
+        public static bool IsMatch([CanBeNull] this string value, [NotNull] string pattern, RegexOptions options, TimeSpan matchTimeout)
+        {
+            return value != null && Regex.IsMatch(value, pattern, options, matchTimeout);
+        }
+    }
+}
