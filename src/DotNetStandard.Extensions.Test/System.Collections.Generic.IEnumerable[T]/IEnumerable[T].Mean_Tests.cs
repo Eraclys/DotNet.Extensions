bfaf9d3b3894388b884e4520799e0de367a6bcdf
Milian Lichere <milian.lichere@tekgent.com>
2018-09-14 14:43:44 +0100

    added IEnumerable[T].Mean

---
+++ src/DotNetStandard.Extensions.Test/System.Collections.Generic.IEnumerable[T]/IEnumerable[T].Mean_Tests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+// ReSharper disable CheckNamespace
+
+namespace DotNetStandard.Extensions.Test
+{
+    [TestFixture]
+    internal sealed class EnumerableExtensions_Mean_Tests
+    {
+        [Test]
+        public void WhenSourceIsNull_ShouldThrowException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ((IEnumerable<int>)null).Mean());
+            Assert.Throws<ArgumentNullException>(() => ((IEnumerable<long>)null).Mean());
+            Assert.Throws<ArgumentNullException>(() => ((IEnumerable<float>)null).Mean());
+            Assert.Throws<ArgumentNullException>(() => ((IEnumerable<double>)null).Mean());
+            Assert.Throws<ArgumentNullException>(() => ((IEnumerable<decimal>)null).Mean());
+        }
+
+        [Test]
+        public void WhenSourceIsEmpty_ShouldThrowException()
+        {
+            Assert.Throws<InvalidOperationException>(() => Enumerable.Empty<int>().Mean());
+            Assert.Throws<InvalidOperationException>(() => Enumerable.Empty<long>().Mean());
+            Assert.Throws<InvalidOperationException>(() => Enumerable.Empty<float>().Mean());
+            Assert.Throws<InvalidOperationException>(() => Enumerable.Empty<double>().Mean());
+            Assert.Throws<InvalidOperationException>(() => Enumerable.Empty<decimal>().Mean());
+        }
+
+        [Test]
+        public void WhenSourceHasValues_ShouldReturnMean()
+        {
+            new int[] {2, 7, 9}.Mean().Should().Be(6);
+            new long[] { 2, 7, 9 }.Mean().Should().Be(6);
+            new float[] { 2, 7, 9 }.Mean().Should().Be(6);
+            new double[] { 2, 7, 9 }.Mean().Should().Be(6);
+            new decimal[] { 2, 7, 9 }.Mean().Should().Be(6);
+        }
+    }
+}
---
+++ src/DotNetStandard.Extensions/System.Collections.Generic.IEnumerable[T]/IEnumerable[T].Mean.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+// ReSharper disable CheckNamespace
+
+namespace DotNetStandard.Extensions
+{
+    public static partial class EnumerableExtensions
+    {
+        public static double Mean([NotNull] this IEnumerable<int> items)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            long sum = 0;
+            long itemCount = 0;
+
+            checked
+            {
+                foreach (var item in items)
+                {
+                    itemCount++;
+                    sum += item;
+                }
+            }
+
+            return itemCount == 0
+                ? throw new InvalidOperationException("Sequence contains no elements")
+                : (double) sum / itemCount;
+        }
+
+        public static double Mean([NotNull] this IEnumerable<long> items)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            long sum = 0;
+            long itemCount = 0;
+
+            checked
+            {
+                foreach (var item in items)
+                {
+                    itemCount++;
+                    sum += item;
+                }
+            }
+
+            return itemCount == 0
+                ? throw new InvalidOperationException("Sequence contains no elements")
+                : (double) sum / itemCount;
+        }
+
+        public static float Mean([NotNull] this IEnumerable<float> items)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            double sum = 0;
+            long itemCount = 0;
+
+            checked
+            {
+                foreach (var item in items)
+                {
+                    itemCount++;
+                    sum += item;
+                }
+            }
+
+            return itemCount == 0
+                ? throw new InvalidOperationException("Sequence contains no elements")
+                : (float)sum / itemCount;
+        }
+
+        public static double Mean([NotNull] this IEnumerable<double> items)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            double sum = 0;
+            long itemCount = 0;
+
+            checked
+            {
+                foreach (var item in items)
+                {
+                    itemCount++;
+                    sum += item;
+                }
+            }
+
+            return itemCount == 0
+                ? throw new InvalidOperationException("Sequence contains no elements")
+                : sum / itemCount;
+        }
+
+        public static decimal Mean([NotNull] this IEnumerable<decimal> items)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            decimal sum = 0;
+            long itemCount = 0;
+
+            checked
+            {
+                foreach (var item in items)
+                {
+                    itemCount++;
+                    sum += item;
+                }
+            }
+
+            return itemCount == 0
+                ? throw new InvalidOperationException("Sequence contains no elements")
+                : sum / itemCount;
+        }
+    }
+}
