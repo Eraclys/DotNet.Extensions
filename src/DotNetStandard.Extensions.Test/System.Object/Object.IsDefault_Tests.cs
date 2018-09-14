a995b1b23d8b16ac3506ee771c15e0faf0e3c158
Milian Lichere <milian.lichere@tekgent.com>
2018-09-14 09:43:56 +0100

    added Object.IsDefault

---
+++ src/DotNetStandard.Extensions.Test/System.Object/Object.IsDefault_Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using FluentAssertions;
+using FsCheck;
+using NUnit.Framework;
+// ReSharper disable CheckNamespace
+
+namespace DotNetStandard.Extensions.Test
+{
+    [TestFixture]
+    internal sealed class ObjectExtensions_IsDefault_Tests
+    {
+        [Test]
+        public void WhenDefaultValue_ShouldReturnTrue()
+        {
+            default(object).IsDefault().Should().BeTrue();
+            default(DateTime).IsDefault().Should().BeTrue();
+            default(int?).IsDefault().Should().BeTrue();
+            default(int).IsDefault().Should().BeTrue();
+        }
+
+        [Test]
+        public void WhenNotDefaultValue_ShouldReturnFalse()
+        {
+            new object().IsDefault().Should().BeFalse();
+            DateTime.Now.IsDefault().Should().BeFalse();
+            ((int?)5).IsDefault().Should().BeFalse();
+            5.IsDefault().Should().BeFalse();
+        }
+    }
+}
---
+++ src/DotNetStandard.Extensions/System.Object/Object.IsDefault.cs
@@ -0,0 +1,13 @@
+﻿using JetBrains.Annotations;
+// ReSharper disable CheckNamespace
+
+namespace DotNetStandard.Extensions
+{
+    public static partial class ObjectExtensions
+    {
+        public static bool IsDefault<T>([CanBeNull] this T value)
+        {
+            return Equals(value, default(T));
+        }
+    }
+}
